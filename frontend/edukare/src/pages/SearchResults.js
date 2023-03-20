189ebfa70a3771f836e5d507ab2c5418a3a3ac0a
Rajdeep nath <rajdeeprajdeep03@gmail.com>
2023-03-21 03:21:23 +0530

    Updated the searchResults page

--- frontend/edukare/src/pages/SearchResults.js
+++ frontend/edukare/src/pages/SearchResults.js
@@ -3,7 +3,7 @@ import Navbaar from '../components/Navbaar'
 
 export default function SearchResults() {
     return (
-        <div style={{ height: "100vh", backgroundColor: "#9cd1cb" }}>
+        <div style={{ backgroundColor: "#9cd1cb" }}>
             <Navbaar />
 
             <div className='col-md-9 mt-4 d-flex flex-column align-items-center justify-content-center mt-3'>
@@ -72,8 +72,54 @@ export default function SearchResults() {
                     </div>
                 </div>
 
-                <div className='col-md-3'>
-                    kuhojk;l
+                <div className='col-md-3 pe-2 ps-1'>
+                    <div className='card' style={{ backgroundColor: "#7aad8c", color: "white" }}>
+                        <div className='card-body'>
+                            <div className='d-flex justify-content-center w-100 mb-4'>
+                                <h3 style={{ color: "gold" }}>LeaderBoard</h3>
+                            </div>
+                            <div>
+                                <table class="table">
+                                    <thead>
+                                        <tr style={{ color: "black" }}>
+                                            <th scope="col">Rank</th>
+                                            <th scope="col">Student Name</th>
+
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr style={{ color: "white" }}>
+                                            <th scope="row"><img src='https://png.pngtree.com/element_pic/17/09/02/edd30932d1c6075b4c0e302476b03e13.png' width={20} /> </th>
+                                            <th scope="row">Rajdeep</th>
+                                        </tr>
+                                        <tr style={{ color: "white" }}>
+                                            <th scope="row"><img src='https://i.pinimg.com/originals/98/ec/c8/98ecc83568ecd89f20fa4e5e47f7fbd4.png' width={20} /> </th>
+                                            <th scope="row">Ashish</th>
+                                        </tr>
+                                        <tr style={{ color: "white" }}>
+                                            <th scope="row"><img src='https://w7.pngwing.com/pngs/919/532/png-transparent-bronze-medal-bronze-medal-gold-medal-medal-medal-gold-material-thumbnail.png' width={20} /> </th>
+                                            <th scope="row">Pranjal</th>
+                                        </tr>
+                                        <tr style={{ color: "white" }}>
+                                            <th scope="row">4</th>
+                                            <th scope="row">Samunder</th>
+                                        </tr>
+
+                                    </tbody>
+                                </table>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            {/* FULL SCREEN VIDEO */}
+            <div className='py-5 d-flex align-items-center justify-content-center' style={{ height: "100vh" }}>
+                <div className='card border w-75' style={{ height: "100%" }}>
+                    <div className='card-body'>
+
+                    </div>
                 </div>
             </div>
 
